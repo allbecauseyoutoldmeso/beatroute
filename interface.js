3edc4b1947cc47eadad57922823c65330f085a94
Magnus Holm <mail@magnusholm.com>
2017-05-09 16:31:15 +0100

    WIP: trying to fetch data from API calls, retrieve ID/uri and pass that into click function on button

--- interface.js
+++ interface.js
@@ -2,18 +2,25 @@ $(document).ready(function() {
 
   var beatroute = new Beatroute();
 
-  // track('6rqhFgbbKwnb9MLmUQDhG6');
+  var queryArray = ['the+clash+london+calling']
 
   function track(queryArray) {
     var artistAndTrackString = queryArray[0]
     var url = "https://api.spotify.com/v1/search?query=" + artistAndTrackString + '&type=track&offset=0&limit=1';
+
     $.get(url, function(data) {
-      $('#song-title').text(data.tracks.items[0].id);
+      id = data.tracks.items[0].id;
     })
   }
 
+
+
+  function goToUrl(id) {
+    window.location.replace('spotify:track:' + id);
+  };
+
   $('#displayTitle').click(function(){
-    track(queryArray);
+    goToUrl();
   });
 
 })
