6ef5833090544821e13cd35d77cb52f14947db60
kittysquee <kittysquee@gmail.com>
2017-05-10 09:31:24 +0100

    Enables app to be opened with selected song

--- interface.js
+++ interface.js
@@ -2,25 +2,23 @@ $(document).ready(function() {
 
   var beatroute = new Beatroute();
 
-  var queryArray = ['the+clash+london+calling']
+  var queryArray = ['the+clash+lost+in+the+supermarket'];
 
   function track(queryArray) {
-    var artistAndTrackString = queryArray[0]
+    var artistAndTrackString = queryArray[0];
     var url = "https://api.spotify.com/v1/search?query=" + artistAndTrackString + '&type=track&offset=0&limit=1';
 
     $.get(url, function(data) {
-      id = data.tracks.items[0].id;
-    })
+      goToUrl(data.tracks.items[0].id);
+    });
   }
-
-
 
   function goToUrl(id) {
     window.location.replace('spotify:track:' + id);
-  };
+  }
 
   $('#displayTitle').click(function(){
-    goToUrl();
+    track(queryArray);
   });
 
-})
+});
