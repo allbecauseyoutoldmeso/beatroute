d11e802d14d27f4e11002d9f142b0916b64a141a
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-05-13 12:31:39 +0100

    update tests for SpotifyApiCall

--- spec/SpotifyApiCallSpec.js
+++ spec/SpotifyApiCallSpec.js
@@ -10,40 +10,43 @@ describe('SpotifyApiCall', function() {
   var halfValidArray = ["雀羅+壊胎", 'the+smiths+this+charming+man'];
   var elevenSongArray = ['blur+song+2', 'blur+song+2', 'blur+song+2', 'blur+song+2', 'blur+song+2', 'blur+song+2', 'blur+song+2', 'blur+song+2', 'blur+song+2', 'blur+song+2', 'blur+song+2'];
 
-  beforeEach(function() {
-      spotifyApiCall = new SpotifyApiCall(testArrayMultiple);
-  });
+  // beforeEach(function() {
+  //     spotifyApiCall = new SpotifyApiCall(testArrayMultiple);
+  // });
 
   it('sends request and receives response', function() {
+    spotifyApiCall = new SpotifyApiCall(testArrayMultiple);
     spotifyApiCall.sendRequest(testString);
     expect(spotifyApiCall.request.status).toEqual(200);
   });
 
-  it('should return the id for testArray-track', function() {
-    expect(spotifyApiCall.getTrackId(SpotifyDemoData())).toEqual('72boGlgSwUK01n44O2tOCv');
-  });
-
   it('creates an array of track ids from the supplied array', function() {
-    spotifyApiCall.lookUpAndStoreTrackIds(testArrayMultiple);
+    spotifyApiCall = new SpotifyApiCall(testArrayMultiple);
+    spotifyApiCall.lookUpAndStoreTrackIds();
     expect(spotifyApiCall.idArray).toEqual(['72boGlgSwUK01n44O2tOCv','1FvDJ9KGxcqwv1utyPL3JZ' ])
   })
 
   it('creates a string of formatted track IDs', function() {
-    expect(spotifyApiCall.generatePlaylistString(testIdArray)).toEqual('spotify%3Atrack%3A72boGlgSwUK01n44O2tOCv,spotify%3Atrack%3A1FvDJ9KGxcqwv1utyPL3JZ');
+    spotifyApiCall = new SpotifyApiCall(testArrayMultiple);
+    spotifyApiCall.lookUpAndStoreTrackIds();
+    expect(spotifyApiCall.generatePlaylistString()).toEqual('spotify%3Atrack%3A72boGlgSwUK01n44O2tOCv,spotify%3Atrack%3A1FvDJ9KGxcqwv1utyPL3JZ');
   });
 
   it('does not try to add tracks that spotify does not recognise', function() {
-    spotifyApiCall.lookUpAndStoreTrackIds(invalidTestArray);
+    spotifyApiCall = new SpotifyApiCall(invalidTestArray);
+    spotifyApiCall.lookUpAndStoreTrackIds();
     expect(spotifyApiCall.idArray).toEqual([])
   })
 
   it('only processes valid tracks', function() {
-    spotifyApiCall.lookUpAndStoreTrackIds(halfValidArray);
+    spotifyApiCall = new SpotifyApiCall(halfValidArray);
+    spotifyApiCall.lookUpAndStoreTrackIds();
     expect(spotifyApiCall.idArray).toEqual(['1FvDJ9KGxcqwv1utyPL3JZ'])
   })
 
   it('only processes 10 tracks', function() {
-    spotifyApiCall.lookUpAndStoreTrackIds(elevenSongArray);
+    spotifyApiCall = new SpotifyApiCall(elevenSongArray);
+    spotifyApiCall.lookUpAndStoreTrackIds();
     expect(spotifyApiCall.idArray.length).toEqual(10)
   })
 
