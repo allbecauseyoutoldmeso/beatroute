49540ce302ba0faf84e24c376ab9d82481a0511f
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-08 10:49:20 +0100

    make procfile

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 
-/**
- * This is an example of a basic node.js script that performs
- * the Implicit Grant oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#implicit_grant_flow
- */
-
-var express = require('express'); // Express web server framework
+var express = require('express');
 var app = express();
-app.use(express.static(__dirname)); //+ '/public'
+app.use(express.static(__dirname)); 
 console.log('Listening on 8888');
 app.listen(process.env.PORT || 8888);
